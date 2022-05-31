968f98784ce3be2fbd803b8ba8ad87e655a8ae9b
MarekPikula <MarekPikula@users.noreply.github.com>
2022-05-31 22:36:06 +0000

    deploy: PocketGourmet/PocketGourmet@6bfbba9cff1733beb412cb53123641d5a888e2af

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "5df71d1196e5697d1078b6f7fb53aff2",
+  "main.dart.js": "0fc48a4bc2e2649b7aee3f6cfb60da91",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
@@ -19,8 +19,8 @@ const RESOURCES = {
 "splash/splash.js": "c6a271349a0cd249bdb6d3c4d12f5dcf",
 "splash/style.css": "12a69284115e7a7d5c2fa66c1f632980",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
-"index.html": "76f0a94c67168bd9b358548e1cd9ffa8",
-"/": "76f0a94c67168bd9b358548e1cd9ffa8",
+"index.html": "ae050429e34112356767adb38af31034",
+"/": "ae050429e34112356767adb38af31034",
 "favicon.png": "036758d104d7d4b2223748aa8da562df",
 "version.json": "2ff178cd1d3a375e5023589ef7dda60b",
 "assets/AssetManifest.json": "ed1bcebc6a66a81baf558e4d0b139321",
@@ -28,7 +28,7 @@ const RESOURCES = {
 "assets/assets/logo.png": "b01cf4538443741fda96283d29469c95",
 "assets/assets/samples/gallery.json": "646f61cafacde33d074735659430ccb7",
 "assets/assets/samples/croissant.png": "1dc8b17cb6496931e7808f543138378e",
-"assets/NOTICES": "c7d9282aea91142b8ccc6d2dcba9172c",
+"assets/NOTICES": "6be0245e514f2cb81399f72dc4488142",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "manifest.json": "b7b7360bc175d4c7cde86289dec413ad",
